5df99a6e7f0d874945ddb3dd45f5815002a51c05
ItsRahim <rahim1605@gmail.com>
2025-03-16 14:27:24 +0000

    removed test controller

--- rgts-pricing-service/src/main/java/com/rahim/pricingservice/controller/PriceController.java
+++ rgts-pricing-service/src/main/java/com/rahim/pricingservice/controller/PriceController.java
@@ -1,18 +1,11 @@
 package com.rahim.pricingservice.controller;
 
-import com.google.protobuf.Empty;
-import com.rahim.proto.protobuf.GoldPriceResponse;
-import com.rahim.proto.protobuf.GoldPriceServiceGrpc;
 import com.rahim.proto.service.IGrpcClientService;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.HashMap;
-import java.util.Map;
 
 /**
  * @author Rahim Ahmed
@@ -24,30 +17,4 @@ import java.util.Map;
 public class PriceController {
     private static final Logger logger = LoggerFactory.getLogger(PriceController.class);
     private final IGrpcClientService grpcClientService;
-
-    @GetMapping("/test")
-    public Map<String, Object> test() {
-        try {
-            GoldPriceResponse goldPriceResponse = grpcClientService.sendRequest(
-                    GoldPriceServiceGrpc::newBlockingStub,
-                    getEmptyRequest(),
-                    GoldPriceServiceGrpc.GoldPriceServiceBlockingStub::getGoldPrice
-            );
-            logger.debug("Received response from gRPC: {}", goldPriceResponse);
-
-            Map<String, Object> response = new HashMap<>();
-            response.put("Source", goldPriceResponse.getSource());
-            response.put("Price", goldPriceResponse.getPrice());
-            response.put("Request Time", goldPriceResponse.getDatetime());
-
-            return response;
-        } catch (Exception e) {
-            logger.error("Error occurred: {}", e.getMessage());
-            return null;
-        }
-    }
-
-    private Empty getEmptyRequest() {
-        return Empty.getDefaultInstance();
-    }
 }
