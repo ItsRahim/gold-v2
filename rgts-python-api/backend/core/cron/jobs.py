d0fade5ffbd6f376cafea71dd2fd6a8d50f0281e
ItsRahim <rahim1605@gmail.com>
2025-03-21 19:59:34 +0000

    updated code to use async

--- rgts-python-api/backend/core/cron/jobs.py
+++ rgts-python-api/backend/core/cron/jobs.py
@@ -2,12 +2,10 @@ import asyncio
 import logging
 from datetime import datetime, UTC
 
-import crython
-
-from backend.util.config import Config
-from backend.infrastructure.kafka_handler import KafkaHandler
 from backend.core.scraper.scraper import get_gold_price
+from backend.infrastructure.kafka_handler import KafkaHandler
 from backend.proto.price_pb2 import GoldPriceInfo
+from backend.util.config import Config
 
 logger = logging.getLogger(__name__)
 
@@ -15,7 +13,7 @@ gold_price_update_topic = Config.get('GOLD_PRICE_UPDATE_TOPIC', 'gold.price.upda
 
 
 async def fetch_and_send():
-    """Fetch gold price and send it to Kafka."""
+    """Fetch gold price and send it to Kafka asynchronously."""
     source_name, price_data = await get_gold_price()
 
     if source_name is not None and price_data is not None:
@@ -27,19 +25,9 @@ async def fetch_and_send():
             datetime=request_time
         )
 
-        message_data = gold_price_info.SerializeToString()
+        await asyncio.to_thread(KafkaHandler.send_message, topic=gold_price_update_topic,
+                                message=gold_price_info.SerializeToString(), delete_cached=True)
 
-        KafkaHandler.send_message(topic=gold_price_update_topic, message=message_data, delete_cached=True)
         logger.info(f"Sent gold price to Kafka: {price_data} at {request_time}")
     else:
         logger.warning("Failed to fetch gold price.")
-
-
-def run_fetch_gold_price():
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(fetch_and_send())
-
-
-@crython.job(expr='@minutely')
-def fetch_gold_price():
-    run_fetch_gold_price()
--- rgts-python-api/main.py
+++ rgts-python-api/main.py
@@ -1,12 +1,14 @@
+import asyncio
 import logging
-import time
 
-import crython
 import uvicorn
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+from apscheduler.triggers.date import DateTrigger
+from apscheduler.triggers.interval import IntervalTrigger
 from fastapi import FastAPI
 
 from backend.core.api.source import router as price_source_router
-from backend.core.cron.jobs import run_fetch_gold_price
+from backend.core.cron.jobs import fetch_and_send
 
 logger = logging.getLogger(__name__)
 
@@ -14,16 +16,32 @@ app = FastAPI()
 
 app.include_router(price_source_router)
 
-if __name__ == '__main__':
-    logger.info("Starting gold price scraper...")
-    crython.start()
-    run_fetch_gold_price()
 
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+async def initialise_cron_jobs():
+    """Start the price job at an interval (e.g., every minute)."""
+    scheduler = AsyncIOScheduler()
+    scheduler.add_job(fetch_and_send, DateTrigger())
+    scheduler.add_job(fetch_and_send, IntervalTrigger(seconds=60))
+    scheduler.start()
 
     try:
         while True:
-            time.sleep(1)
-    except KeyboardInterrupt:
+            await asyncio.sleep(1)
+    except (KeyboardInterrupt, SystemExit):
         logger.info("Stopping scheduler...")
-        crython.stop()
+        scheduler.shutdown()
+
+
+async def run_app():
+    """Run FastAPI app along with the price job."""
+    asyncio.create_task(initialise_cron_jobs())
+
+    # Run the FastAPI application using uvicorn
+    config = uvicorn.Config(app, host="0.0.0.0", port=8000)
+    server = uvicorn.Server(config)
+    await server.serve()
+
+
+if __name__ == "__main__":
+    logger.info("Starting gold price scraper...")
+    asyncio.run(run_app())
