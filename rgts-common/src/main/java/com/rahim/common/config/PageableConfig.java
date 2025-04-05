7de699122bdd8c851e4ca172ad0316c240457572
ItsRahim <rahim1605@gmail.com>
2025-04-05 12:15:20 +0100

    added pageebale config

---
+++ rgts-common/src/main/java/com/rahim/common/config/PageableConfig.java
@@ -0,0 +1,12 @@
+package com.rahim.common.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
+
+/**
+ * @created 05/04/2025
+ * @author Rahim Ahmed
+ */
+@Configuration
+@EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
+public class PageableConfig {}
---
+++ rgts-pricing-service/src/test/java/com/rahim/pricingservice/service/QueryGoldTypeServiceTest.java
@@ -0,0 +1,11 @@
+package com.rahim.pricingservice.service;
+
+import com.rahim.pricingservice.BaseUnitTest;
+
+/**
+ * @created 05/04/2025
+ * @author Rahim Ahmed
+ */
+public class QueryGoldTypeServiceTest extends BaseUnitTest {
+
+}
