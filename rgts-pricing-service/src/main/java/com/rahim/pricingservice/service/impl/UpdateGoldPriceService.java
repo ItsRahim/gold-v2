811cee63416b0a24544be20ecfe8467cb341e8cd
ItsRahim <rahim1605@gmail.com>
2025-05-03 20:40:48 +0100

    rework of tables

--- rgts-pricing-service/src/main/java/com/rahim/pricingservice/service/impl/UpdateGoldPriceService.java
+++ rgts-pricing-service/src/main/java/com/rahim/pricingservice/service/impl/UpdateGoldPriceService.java
@@ -1,6 +1,8 @@
 package com.rahim.pricingservice.service.impl;
 
 import com.rahim.pricingservice.dto.grpc.GoldPriceUpdateDTO;
+import com.rahim.pricingservice.entity.GoldPrice;
+import com.rahim.pricingservice.entity.GoldType;
 import com.rahim.pricingservice.repository.GoldPriceRepository;
 import com.rahim.pricingservice.service.IUpdateGoldPriceService;
 import com.rahim.pricingservice.util.GoldCaratUtil;
@@ -8,6 +10,11 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import javax.measure.Quantity;
+import javax.measure.quantity.Mass;
+import java.math.BigDecimal;
+import java.util.List;
 
 /**
  * @created 03/05/2025
@@ -22,5 +29,15 @@ public class UpdateGoldPriceService implements IUpdateGoldPriceService {
   private final GoldCaratUtil goldCaratUtil;
 
   @Override
-  public void updateGoldPrice(GoldPriceUpdateDTO goldPriceUpdateDTO) {}
+  public void updateGoldPrice(GoldPriceUpdateDTO goldPriceUpdateDTO) {
+    BigDecimal currentPrice = goldPriceUpdateDTO.getPrice();
+
+    List<GoldPrice> allGoldPrices = goldPriceRepository.findAll();
+    for (GoldPrice goldPrice : allGoldPrices) {
+      GoldType goldType = goldPrice.getGoldType();
+      String carat = goldType.getCarat();
+      BigDecimal purity = goldCaratUtil.getPurity(carat);
+      BigDecimal weight = goldType.getWeight();
+    }
+  }
 }
