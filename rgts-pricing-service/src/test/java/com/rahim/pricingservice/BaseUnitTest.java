a1beeedf9461c6931b9b692fd8cb96261f5eb827
ItsRahim <rahim1605@gmail.com>
2025-05-04 14:23:28 +0100

    added dirtie contentext

--- rgts-pricing-service/src/test/java/com/rahim/pricingservice/BaseUnitTest.java
+++ rgts-pricing-service/src/test/java/com/rahim/pricingservice/BaseUnitTest.java
@@ -1,6 +1,7 @@
 package com.rahim.pricingservice;
 
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -11,4 +12,5 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional
 @SpringBootTest
 @TestPropertySource("classpath:application.yml")
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 public abstract class BaseUnitTest {}
