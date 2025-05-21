98e0d6df08e200650bb62f29936eb54ba5440553
ItsRahim <rahim1605@gmail.com>
2025-05-21 15:15:15 +0100

    another attempt to fix tests

--- rgts-pricing-service/src/test/java/com/rahim/pricingservice/BaseControllerTest.java
+++ rgts-pricing-service/src/test/java/com/rahim/pricingservice/BaseControllerTest.java
@@ -1,12 +1,23 @@
 package com.rahim.pricingservice;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.rahim.cachemanager.service.RedisService;
 import com.rahim.common.handler.ApiExceptionHandler;
+import com.rahim.pricingservice.entity.GoldPrice;
+import com.rahim.pricingservice.entity.GoldPurity;
+import org.junit.jupiter.api.BeforeEach;
+import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.math.BigDecimal;
+import java.time.Instant;
+
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
 
 /**
  * @author Rahim Ahmed
@@ -18,7 +29,16 @@ import org.springframework.transaction.annotation.Transactional;
 @TestPropertySource("classpath:application.yml")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public abstract class BaseControllerTest {
+  @Mock RedisService redisService;
+
   private final ObjectMapper objectMapper = new ObjectMapper();
+
+  @BeforeEach
+  void setUp() {
+    GoldPurity goldPurity = new GoldPurity(1, "22K", 22, 24, false);
+    when(redisService.getValue(anyString()))
+        .thenReturn(new GoldPrice(1, goldPurity, BigDecimal.valueOf(100), Instant.now()));
+  }
 
   public String requestToJson(Object object) throws Exception {
     return objectMapper.writeValueAsString(object);
