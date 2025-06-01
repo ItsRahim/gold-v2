0b35f321abc3747f981cd3b04e61bfb61747a7e4
ItsRahim <rahim1605@gmail.com>
2025-06-01 10:34:30 +0100

    Implemented Zipkin, Prometheus, Grafana and API Gateway (#98)
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

---
+++ rgts-gateway-service/src/main/java/com/rahim/gatewayservice/config/GatewayConfig.java
@@ -0,0 +1,36 @@
+package com.rahim.gatewayservice.config;
+
+import static com.rahim.gatewayservice.constants.UriConstants.*;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.GatewayFilterSpec;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @created 01/06/2025
+ * @author Rahim Ahmed
+ */
+@Configuration
+@RequiredArgsConstructor
+public class GatewayConfig {
+  @Bean
+  public RouteLocator routeLocator(RouteLocatorBuilder builder) {
+    return builder
+        .routes()
+        .route(
+            PRICING_SERVICE,
+            r ->
+                r.path(PRICING_PATHS[0], PRICING_PATHS[1])
+                    .filters(f -> f.rewritePath(PRICING_REWRITE_REGEX, PRICING_REWRITE_REPLACEMENT))
+                    .uri(PRICING_SERVICE_URI))
+        .build();
+  }
+
+  private GatewayFilterSpec applyPricingServiceFilters(GatewayFilterSpec filters) {
+    return filters.stripPrefix(3);
+    // .filter(jwtAuthenticationFilter())
+  }
+}
