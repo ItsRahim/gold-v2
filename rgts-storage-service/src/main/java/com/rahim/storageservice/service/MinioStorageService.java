16dc5df2e7a116f6e209f87dadbdf837f3a6586c
ItsRahim <rahim1605@gmail.com>
2025-06-27 17:03:39 +0100

    finished implementation of remaining methods

--- rgts-storage-service/src/main/java/com/rahim/storageservice/service/MinioStorageService.java
+++ rgts-storage-service/src/main/java/com/rahim/storageservice/service/MinioStorageService.java
@@ -1,6 +1,6 @@
 package com.rahim.storageservice.service;
 
-import io.minio.MinioClient;
+import io.minio.*;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -19,29 +19,93 @@ public class MinioStorageService implements StorageService {
   private final MinioClient minioClient;
 
   @Override
-  public void createBucket(String bucketName) {}
-
-  @Override
-  public void deleteBucket(String bucketName) {}
-
-  @Override
-  public boolean bucketExists(String bucketName) {
-    return false;
+  public void createBucket(String bucketName) {
+    try {
+      minioClient.makeBucket(MakeBucketArgs.builder().bucket(bucketName).build());
+    } catch (Exception e) {
+      log.error("Failed to create bucket '{}': {}", bucketName, e.getMessage(), e);
+    }
   }
 
   @Override
-  public void upload(String bucketName, String objectKey, MultipartFile file) {}
+  public void deleteBucket(String bucketName) {
+    try {
+      minioClient.deleteBucketTags(DeleteBucketTagsArgs.builder().bucket(bucketName).build());
+    } catch (Exception e) {
+      log.error("Failed to delete bucket tags for '{}': {}", bucketName, e.getMessage(), e);
+    }
+  }
+
+  @Override
+  public boolean bucketExists(String bucketName) {
+    try {
+      return minioClient.bucketExists(BucketExistsArgs.builder().bucket(bucketName).build());
+    } catch (Exception e) {
+      log.error("Failed to check if bucket '{}' exists: {}", bucketName, e.getMessage(), e);
+      return false;
+    }
+  }
+
+  @Override
+  public void upload(String bucketName, String objectKey, MultipartFile file) {
+    try (InputStream inputStream = file.getInputStream()) {
+      minioClient.putObject(
+          PutObjectArgs.builder().bucket(bucketName).object(objectKey).stream(
+                  inputStream, file.getSize(), -1)
+              .contentType(file.getContentType())
+              .build());
+      log.info("Successfully uploaded object '{}' to bucket '{}'", objectKey, bucketName);
+    } catch (Exception e) {
+      log.error(
+          "Failed to upload object '{}' to bucket '{}': {}",
+          objectKey,
+          bucketName,
+          e.getMessage(),
+          e);
+    }
+  }
 
   @Override
   public InputStream download(String bucketName, String objectKey) {
-    return null;
+    try {
+      return minioClient.getObject(
+          GetObjectArgs.builder().bucket(bucketName).object(objectKey).build());
+    } catch (Exception e) {
+      log.error(
+          "Failed to download object '{}' from bucket '{}': {}",
+          objectKey,
+          bucketName,
+          e.getMessage(),
+          e);
+      return null;
+    }
   }
 
   @Override
   public boolean objectExists(String bucketName, String objectKey) {
-    return false;
+    try {
+      minioClient.statObject(StatObjectArgs.builder().bucket(bucketName).object(objectKey).build());
+      return true;
+    } catch (Exception e) {
+      log.warn(
+          "Object '{}' does not exist in bucket '{}': {}", objectKey, bucketName, e.getMessage());
+      return false;
+    }
   }
 
   @Override
-  public void deleteObject(String bucketName, String objectKey) {}
+  public void deleteObject(String bucketName, String objectKey) {
+    try {
+      minioClient.removeObject(
+          RemoveObjectArgs.builder().bucket(bucketName).object(objectKey).build());
+      log.info("Successfully deleted object '{}' from bucket '{}'", objectKey, bucketName);
+    } catch (Exception e) {
+      log.error(
+          "Failed to delete object '{}' from bucket '{}': {}",
+          objectKey,
+          bucketName,
+          e.getMessage(),
+          e);
+    }
+  }
 }
